0808de99d85f7daef63dca26daa740fe9819aca2
Polina Butrina <40416741+polinabutrina@users.noreply.github.com>
2021-09-27 11:32:01 -0700

    time series function

---
+++ R/create-time-series.R
@@ -0,0 +1,85 @@
+
+#' Time Series Visualization
+#'
+#' Generate Time Series Charts for Census Data
+#'
+#' @param table Data frame of census data by Counties or Region.
+#' @param variable Character string as Census code for a variable to plot.
+#' @param ts.title Character. Chart title. If not specified, the title will be based on a variable name given by Census.
+#' @param print.table Boolean. If TRUE table will be returned.
+#'
+#' @author Polina Butrina
+#'
+#' @return a time series chart or table.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#'
+#' @examples
+#' library(rlang)
+#' library(dplyr)
+#' library(rlang)
+#' library(scales)
+#' library(stringr)
+#' library(psrccensus)
+#' 
+#' \dontrun{
+#' Sys.getenv("CENSUS_API_KEY")
+#' }
+#'
+#' ts.table = get_acs_recs(geography = 'county', table.names = c('B01001'), years=c(2019,2018,2017),
+#'                     acs.type = 'acs5')
+
+#'
+#' get_time_series(ts.table, "B01001_002")
+#'
+#' @export
+
+library(devtools)
+library(psrccensus)
+library(rlang)
+library(tidyverse)
+library(scales)
+library(stringr)
+
+
+get_time_series  = function(table,
+                            variable,
+                            ts.title = NULL,
+                            print.table = FALSE) {
+  variable = enquo(variable)
+  
+  df_for_plot = table %>%
+    dplyr::rename_at(dplyr::vars(matches("value")), function(x)
+      "estimate") %>%
+    dplyr::mutate(year = as.character(year)) %>%
+    dplyr::filter(variable == (!!variable))
+  
+  if (print.table == TRUE) {
+    df_for_plot = df_for_plot %>%
+      select(name, variable, estimate, moe, label, year)
+    return(df_for_plot)
+  }
+  
+  if (missing(ts.title)) {
+    ts.title = str_replace_all(df_for_plot$label[1], "[!:]", " ")
+    ts.title = str_replace_all(ts.title, "   ", " ")
+  }
+  
+  legend.title = df_for_plot$census_geography[1]
+  
+  m = ggplot(df_for_plot, aes(
+    x = year,
+    y = estimate,
+    group = name,
+    color = name
+  )) +
+    geom_line(size = 1) +
+    xlab("") +
+    ylab("") +
+    geom_point() +
+    ggtitle(ts.title) +
+    scale_y_continuous(labels = scales::comma) +
+    labs(color = legend.title)
+  return (m)
+}
