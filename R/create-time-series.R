1246d14d4b7d5f55b364c5440cc39fc504c03c2e
Suzanne Childress <childresssuzanne@gmail.com>
2021-09-29 11:02:26 -0700

    updates to documentation, a few fixes

--- R/create-time-series.R
+++ R/create-time-series.R
@@ -1,9 +1,9 @@
 
 #' Time Series Visualization
 #'
-#' Generate Time Series Charts for Census Data
+#' Generate Time Series Charts for Census or ACS Data
 #'
-#' @param table Data frame of census data by Counties or Region.
+#' @param table Data frame of census or ACS data by Counties or Region.
 #' @param variable Character string as Census code for a variable to plot.
 #' @param ts.title Character. Chart title. If not specified, the title will be based on a variable name given by Census.
 #' @param print.table Boolean. If TRUE table will be returned.
@@ -22,7 +22,7 @@
 #' library(scales)
 #' library(stringr)
 #' library(psrccensus)
-#' 
+#'
 #' \dontrun{
 #' Sys.getenv("CENSUS_API_KEY")
 #' }
@@ -34,52 +34,43 @@
 #' get_time_series(ts.table, "B01001_002")
 #'
 #' @export
-
-library(devtools)
-library(psrccensus)
-library(rlang)
-library(tidyverse)
-library(scales)
-library(stringr)
-
-
 get_time_series  = function(table,
                             variable,
                             ts.title = NULL,
                             print.table = FALSE) {
-  variable = enquo(variable)
-  
+  variable = dplyr::enquo(variable)
+
   df_for_plot = table %>%
     dplyr::rename_at(dplyr::vars(matches("value")), function(x)
       "estimate") %>%
     dplyr::mutate(year = as.character(year)) %>%
     dplyr::filter(variable == (!!variable))
-  
+
   if (print.table == TRUE) {
     df_for_plot = df_for_plot %>%
       select(name, variable, estimate, moe, label, year)
     return(df_for_plot)
   }
-  
+
   if (missing(ts.title)) {
     ts.title = str_replace_all(df_for_plot$label[1], "[!:]", " ")
     ts.title = str_replace_all(ts.title, "   ", " ")
   }
-  
+
   legend.title = df_for_plot$census_geography[1]
-  
-  m = ggplot(df_for_plot, aes(
+
+  m = ggplot2::ggplot(df_for_plot, ggplot2::aes(
     x = year,
     y = estimate,
     group = name,
     color = name
   )) +
-    geom_line(size = 1) +
-    xlab("") +
-    ylab("") +
-    geom_point() +
-    ggtitle(ts.title) +
-    scale_y_continuous(labels = scales::comma) +
-    labs(color = legend.title)
+    ggplot2::geom_line(size = 1) +
+    ggplot2::xlab("") +
+    ggplot2::ylab("") +
+    ggplot2::geom_point() +
+    ggplot2::ggtitle(ts.title) +
+    ggplot2::scale_y_continuous(labels = scales::comma) +
+    ggplot2::labs(color = legend.title)
   return (m)
 }
