23478d56de44acc066015297c6d2852adfb2a6a6
Christy Lam <cyyllam@gmail.com>
2021-09-30 15:57:31 -0700

    revised get_decennial_recs for block group geog, year to years

--- R/get-decennial-recs.R
+++ R/get-decennial-recs.R
@@ -4,10 +4,10 @@ utils::globalVariables("GEOID")
 #'
 #' Generate decennial estimates for multiple tables by multiple tracts and/or counties
 #'
-#' @param geography A character string as either 'tract', 'county', 'msa', or 'place'.
+#' @param geography A character string as either 'tract', 'county', 'block group'.
 #' @param counties A character string or vector of counties. Defaults to PSRC counties.
 #' @param table_codes A character string or vector of Census table codes.
-#' @param year Numeric value. A decennial year equal or greater than 2010
+#' @param years Numeric or a vector of numeric years. A decennial year or years equal or greater than 2010.
 #' @param state A character string state abbreviation
 #'
 #' @author Christy Lam
@@ -15,8 +15,8 @@ utils::globalVariables("GEOID")
 #' @return a tibble of decennial estimates by either tracts in a county/counties for selected table codes. Does not include
 #' variable names.
 #'@keywords internal
-get_decennial_tract_county <- function(geography, counties = c('King', 'Kitsap', 'Pierce', 'Snohomish'),
-                                       table_codes, year, state = 'WA') {
+get_decennial_tract_county_bg <- function(geography, counties = c('King', 'Kitsap', 'Pierce', 'Snohomish'),
+                                       table_codes, years, state = 'WA') {
   get_decennial_geogs <- purrr::partial(tidycensus::get_decennial,
                                         geography = geography,
                                         state = state,
@@ -30,6 +30,16 @@ get_decennial_tract_county <- function(geography, counties = c('King', 'Kitsap',
     df <- purrr::reduce(all_geogs, dplyr::bind_rows)
     ifelse(is.null(dfs), dfs <- df, dfs <- dplyr::bind_rows(dfs, df))
   }
+
+  # create regional summary for county geography
+  if(geography == 'county' & identical(unique(dfs$GEOID), c('53033', '53035', '53053', '53061'))) {
+    region <- dfs %>%
+      dplyr::group_by(.data$variable) %>%
+      dplyr::summarise(value = sum(.data$value)) %>%
+      dplyr::mutate(GEOID = 'REGION', NAME = 'Region')
+    dfs <- dplyr::bind_rows(dfs, region)
+  }
+
   return(dfs)
 }
 #'
@@ -38,7 +48,7 @@ get_decennial_tract_county <- function(geography, counties = c('King', 'Kitsap',
 #' Generate decennial estimates for multiple tables by MSA(s).
 #'
 #' @param table_codes A character string or vector of Census table codes.
-#' @param year Numeric value. A decennial year equal or greater than 2010
+#' @param years Numeric or a vector of numeric years. A decennial year or years equal or greater than 2010.
 #' @param fips Character value. Single code or vector of MSA fips codes.
 #'
 #' @author Christy Lam
@@ -46,7 +56,7 @@ get_decennial_tract_county <- function(geography, counties = c('King', 'Kitsap',
 #' @return a tibble of decennial estimates by MSA(s) for selected table codes. Does not include
 #' variable names.
 #'@keywords internal
-get_decennial_msa <- function(table_codes, year, fips = NULL) {
+get_decennial_msa <- function(table_codes, years, fips = NULL) {
   msa_geog <- 'metropolitan statistical area/micropolitan statistical area'
 
   dfs <- NULL
@@ -67,7 +77,7 @@ get_decennial_msa <- function(table_codes, year, fips = NULL) {
 #' Generate decennial estimates for multiple tables by place(s).
 #'
 #' @param table_codes A character string or vector of Census table codes.
-#' @param year Numeric value. A decennial year equal or greater than 2010
+#' @param years Numeric or a vector of numeric years. A decennial year or years equal or greater than 2010.
 #' @param fips Character value. Single code or vector of place fips codes.
 #' @param state A character string state abbreviation
 #'
@@ -94,46 +104,50 @@ get_decennial_place <- function(table_codes, year, fips = NULL, state = 'WA') {
 #'
 #' Generate decennial estimates for multiple tables by tracts, counties, MSAs, or places.
 #' Currently only working for SF1 tables.
-#' @param geography A character string as either 'tract', 'county', 'msa', or 'place'.
+#' @param geography A character string as either 'tract', 'county', 'block group', 'msa', or 'place'.
 #' @param counties A character string or vector of counties. Defaults to PSRC counties.
 #' @param table_codes A character string or vector of Census table codes,
 #' the table code will be padded with 0s such as "H001", as opposed to "H1
-#' @param year Numeric. A decennial year equal or greater than 2010.
+#' @param years Numeric or a vector of numeric years. A decennial year or years equal or greater than 2010.
 #' @param fips Character. Single code or vector of either MSA or place fips codes.
 #'
 #' @author Christy Lam
 #'
 #' @return a tibble of decennial estimates by selected geography for selected table codes. Includes variable names.
 #' @examples
-#' \dontrun{
-#' Sys.getenv("CENSUS_API_KEY")}
-#' tbl_names <- paste0('PCT020', LETTERS[1:6])
-#' get_decennial_recs(geography = 'county', table_codes = tbl_names, year = 2010)
 #'
-#' get_decennial_recs(geography = 'tract', table_codes = tbl_names, year = 2010)
+#' Sys.getenv("CENSUS_API_KEY")
+#' tbl_names <- paste0('PCT020', LETTERS[1:6])
+#' get_decennial_recs(geography = 'county', table_codes = tbl_names, years = 2010)
+#'
+#' get_decennial_recs(geography = 'tract', table_codes = tbl_names, years = 2010)
 #'
 #' get_decennial_recs(geography = 'place',
 #'                    table_codes = 'PCT013',
-#'                    year = 2010,
+#'                    years = 2010,
 #'                    fips = c("5363000", "5308850"))
 #'
 #' get_decennial_recs(geography = 'msa',
 #'                    table_codes = c("H001", "P001"),
-#'                    year = 2010,
+#'                    years = 2010,
 #'                    fips = c('42660', "28420"))
+#'
+#' get_decennial_recs(geography = 'block group',
+#'                    table_codes = c('H001', 'H006'),
+#'                    years = 2010)
 #' @export
-get_decennial_recs <- function(geography, counties = c('King', 'Kitsap', 'Pierce', 'Snohomish'), table_codes, year,
+get_decennial_recs <- function(geography, counties = c('King', 'Kitsap', 'Pierce', 'Snohomish'), table_codes, years,
                                fips = NULL) {
 
-  if(geography %in% c('tract', 'county')) {
-    dfs <- get_decennial_tract_county(geography = geography, table_codes = table_codes, year = year)
+  if(geography %in% c('tract', 'county', 'block group')) {
+    dfs <- get_decennial_tract_county_bg(geography = geography, table_codes = table_codes, years = years)
   } else if (geography == 'msa'){
-    dfs <- get_decennial_msa(table_codes, year, fips = fips)
+    dfs <- get_decennial_msa(table_codes, years, fips = fips)
   } else if(geography == 'place') {
-    dfs <- get_decennial_place(table_codes, year, fips = fips)
+    dfs <- get_decennial_place(table_codes, years, fips = fips)
   }
   # add labels
-  vars <- tidycensus::load_variables(year, "sf1")
+  vars <- tidycensus::load_variables(years, "sf1")
   df_join <- dplyr::left_join(dfs, vars, by = c("variable" = "name"))
   return(df_join)
 }
