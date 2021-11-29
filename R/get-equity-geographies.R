9f6cede9b76f4e753c62892b16874f4272097b17
Suzanne Childress <childresssuzanne@gmail.com>
2021-11-29 10:41:15 -0800

    Revert "random stuff i don't want now"
    This reverts commit fac5ff041ea6871857f1dd63f0e5b03d59378a06.

--- R/get-equity-geographies.R
+++ R/get-equity-geographies.R
@@ -19,29 +19,14 @@
 #' \dontrun{
 #' Sys.getenv("CENSUS_API_KEY")
 #' }
-#' elmer_connection <- DBI::dbConnect(odbc::odbc(),
+#' elmer_connection <- dbConnect(odbc::odbc(),
 #'                              driver = "SQL Server",
 #'                              server = "AWS-PROD-SQL\\Sockeye",
 #'                              database = "Elmer",
 #'                              trusted_connection = "yes")
 #'
 #' poverty_tracts<-get_equity_geographies(elmer_connection, 'equity_geog_vs_reg_total', 'poverty_equity_geographies','2018')
-#' means_transport<-get_acs_recs('tract',table.names=c('S0801_C01'),   years=c(2019),
-#'                              acs.type = 'acs1')
-
-
-#' tot_workers<-means_transport%>%filter(variable=='S0801_C01_001')
-#' transit<-means_transport%>%filter(variable=='S0801_C01_009')
-
-#' poverty_wrkrs<-merge(poverty_tracts, tot_workers, by.x='geoid', by.y='GEOID')
-#' poverty_transit<-merge(poverty_wrkrs, transit, by.x='geoid', by.y='GEOID')
-
-#' poverty_transit_calcs<-poverty_transit%>%mutate(transit=estimate.x*estimate.y/100)%>%
-#' mutate(workers=estimate.x)%>%select('geoid', 'transit', 'workers', 'equity_geog_vs_reg_total')
-#' poverty_transit_summary<-poverty_transit_calcs%>%group_by(equity_geog_vs_reg_total)%>%
-#' summarise(sum_workers=sum(workers), sum_transit=sum(transit, na.rm=TRUE))%>%
-#' mutate(transit_share=sum_transit/sum_workers)
-#' poverty_transit_summary
+#' head(poverty_tracts)
 #' @export
 get_equity_geographies<- function(elmer_connection, equity_type='equity_geog_vs_50_percent',equity_group='racial_equity_geographies',year='2016'){
   query_geo=paste0('SELECT geoid, ', equity_type, ' FROM census.', equity_group,'(',year, ', \'Tract\')')
