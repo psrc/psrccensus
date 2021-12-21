70d3f39762c697e64971ea29ef27e06ae18b1506
Suzanne Childress <childresssuzanne@gmail.com>
2021-12-20 16:15:00 -0800

    Update get-psrc-pums.R

--- R/get-psrc-pums.R
+++ R/get-psrc-pums.R
@@ -9,8 +9,8 @@
 #'
 #' @return the data.table with the input variable adjusted
 #'
-#' @import(data.table)
 #' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
 
 adjust_inflation <-function(dt, dollar_var, adj_var){
   dt %<>% .[, (dollar_var):=round(as.numeric(gsub(",", "", get(dollar_var))) * as.numeric(get(adj_var)),0)]
@@ -28,8 +28,8 @@ adjust_inflation <-function(dt, dollar_var, adj_var){
 #'
 #' @return the data.table with dollar value adjusted
 #'
-#' @import(data.table)
 #' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
 
 adjust_dollars <- function(dt, dollar_var){
   if(dollar_var %chin% c("FINCP","HINCP","INTP","OIP","PAP","PERNP","PINCP","RETP","SEMP","SSIP","SSP","WAGP")){
@@ -52,7 +52,6 @@ adjust_dollars <- function(dt, dollar_var){
 #'
 #' @return the filtered data.table
 #'
-#' @import(data.table)
 #' @importFrom magrittr %<>%
 #' @importFrom magrittr %>%
 
@@ -72,7 +71,6 @@ clip2region <- function(dt){
 #'
 #' @return the filtered data.table
 #'
-#' @import(data.table)
 #' @importFrom magrittr %<>%
 #' @importFrom magrittr %>%
 
@@ -99,14 +97,12 @@ add_county <- function(dt){
 #'
 #' @return the filtered data.table
 #'
-#' @import(data.table)
 #' @importFrom magrittr %<>%
 #' @importFrom magrittr %>%
-
 psrc_pums_groupvar <- function(span, dyear, group_var, tbl_ref, bin_defs=NULL){
   dt   <- tidycensus::get_pums(variables=c(group_var,"PUMA","ADJINC","ADJHSG"), state="WA",
                    survey=paste0("acs", span), year=dyear, recode=if(dyear>2016){TRUE}else{FALSE}) %>% # Recode isn't available prior to 2017
-    setDT() %>% clip2region() %>% adjust_dollars(group_var)
+    data.table::setDT() %>% clip2region() %>% adjust_dollars(group_var)
   if(exists("df$SPORDER") & tbl_ref!="person"){
     dt %<>% .[SPORDER==1] %>% .[, SPORDER:=NULL]                                                   # If target variable is household and group variable is person,
   }                                                                                                # uses 'householder' attributes, per convention (albeit odd)
@@ -131,9 +127,8 @@ psrc_pums_groupvar <- function(span, dyear, group_var, tbl_ref, bin_defs=NULL){
 #'
 #' @return the filtered data.table
 #'
-#' @import(data.table)
+#' @importFrom magrittr %<>%
 #' @importFrom magrittr %>%
-
 psrc_pums_targetvar <- function(span, dyear, target_var, tbl_ref){
   dt <- tidycensus::get_pums(variables=c(target_var,"PUMA","ADJINC","ADJHSG"), state="WA",         # Include inflation adjustment fields
                  survey=paste0("acs", span), year=dyear, recode=if(dyear>2016){TRUE}else{FALSE},   # Recode unavailable prior to 2017
@@ -155,40 +150,55 @@ psrc_pums_targetvar <- function(span, dyear, target_var, tbl_ref){
 #' @author Michael Jensen
 #'
 #' @return A srvyr object ready for summation.
-#'
-#' @import(data.table)
 #' @importFrom magrittr %<>%
 #' @importFrom magrittr %>%
+#' @import data.table
 #' @examples
+#' library(dplyr)
+#' library(srvyr)
+#' \dontrun{
 #' Sys.getenv("CENSUS_API_KEY")}
-# get_psrc_pums("region", 1, 2019, "AGEP", "SEX")
+#'
+#' get_psrc_pums("region", 1, 2019, "AGEP", "SEX")
 #' @export
 get_psrc_pums <- function(span, dyear, target_var, group_var=NULL, bin_defs=NULL){
   varlist       <- c(target_var)
-  pums_vars     <- tidycensus::pums_variables %>% setDT() %>% .[year==dyear & survey==paste0("acs", span)]     # Retrieve variable definitions
-  tbl_ref       <- pums_vars[var_code==target_var, unique(level)]                                  # Table corresponding to unit of analysis (for rep weights)
-  key_ref       <- pums_vars[var_code==group_var, unique(level)]                                   # Table corresponding to grouping variable (for join)
+  pums_vars     <- tidycensus::pums_variables %>% data.table::setDT() %>% dplyr::filter(year==dyear & survey==paste0("acs", span) )   # Retrieve variable definitions
+  tbl_ref       <- pums_vars%>% dplyr::filter(var_code==target_var) %>% dplyr::select(level) %>% dplyr::distinct()                                  # Table corresponding to unit of analysis (for rep weights)
+
+  if(!is.null(group_var)){
+  key_ref       <- pums_vars%>% dplyr::filter(var_code==group_var) %>% dplyr::select(level) %>% dplyr::distinct()
+  # Table corresponding to grouping variable (for join)
   dt_key        <- if(tbl_ref=="person" & key_ref!="housing"){c("SERIALNO","SPORDER")
-                    }else{"SERIALNO"}                                                              # To match join
+  }else{"SERIALNO"}
+  }
+
+  else{
+    dt_key<-"SERIALNO"
+  }
+
+
+# To match join
   rwgt_ref      <- if(tbl_ref=="person"){"PWGTP"}else{"WGTP"}
-  dt <- psrc_pums_targetvar(span, dyear, target_var, tbl_ref) %>% add_county()                     # Target variable via API
+  dt <- psrc_pums_targetvar(span, dyear, target_var, tbl_ref) %>% add_county() %>%                 # Target variable via API
+    data.table::setkeyv(dt_key)
   rw <- colnames(dt) %>% .[grep(paste0(rwgt_ref,"\\d+"),.)]
   if(!is.null(group_var)){
     groupvar_label <- paste0(group_var,"_label")
     varlist %<>% c(groupvar_label)
     group_var_dt <- psrc_pums_groupvar(span, dyear, group_var, tbl_ref, bin_defs=NULL) %>%         # Grouping variable via API\
-      setkeyv(dt_key)
-    dt %<>% setkeyv(dt_key) %>% .[group_var_dt, (groupvar_label):=get(groupvar_label), on=key(.)]  # Link data tables
+      data.table::setkeyv(dt_key)
+    dt %<>% .[group_var_dt, (groupvar_label):=get(groupvar_label), on=key(.)]                      # Link data tables
   }
-  dt %<>% setDF() %>%
-    srvyr::as_survey_rep(variables=all_of(varlist),                                                # Create srvyr object with replication weights for MOE
-                  weights=all_of(rwgt_ref),
-                  repweights=all_of(rw),
+  dt %<>% data.table::setDF() %>%
+    srvyr::as_survey_rep(variables=tidyselect::all_of(varlist),                                                # Create srvyr object with replication weights for MOE
+                  weights=tidyselect::all_of(rwgt_ref),
+                  repweights=tidyselect::all_of(rw),
                   combined_weights=TRUE,
                   mse=TRUE,
                   type="other",
                   scale=4/80,
-                  rscale=length(all_of(rw)))
+                  rscale=length(tidyselect::all_of(rw)))
   return(dt)
 }
 
@@ -208,32 +218,26 @@ get_psrc_pums <- function(span, dyear, target_var, group_var=NULL, bin_defs=NULL
 #'
 #' @return A table with the variable names, desired statistic, and margin of error
 #'
-#' @import(data.table)
-#' @import(srvyr)
-#' @import(dplyr)
-#' @importFrom rlang sym
 #' @importFrom magrittr %<>%
 #' @importFrom magrittr %>%
-
+#' @importFrom rlang sym
+#' @import srvyr
 psrc_pums_stat <- function(stat_type, geo_scale, span, dyear, target_var, group_var=NULL, bin_defs=NULL){
-  result_name <- sym(stat_type)
+  result_name <- rlang::sym(stat_type)
   srvyrf_name <- as.name(paste0("survey_",stat_type))
   se_name     <- paste0(stat_type,"_se")
   moe_name    <- paste0(stat_type,"_moe")
   df <- get_psrc_pums(span, dyear, target_var, group_var, bin_defs)
-  if(!is.null(group_var) & stat_type!="ratio"){dt %<>% group_by(!!as.name(groupvar_label))}
-  if(geo_scale=="county"){df %<>% group_by(COUNTY, .add=TRUE)}
-  rs <- summarize(df, !!result_name:=(as.function(!!srvyrf_name)(
+  if(!is.null(group_var) & stat_type!="ratio"){
+  groupvar_label <- paste0(group_var,"_label")
+  df %<>% dplyr::group_by(.data[[groupvar_label]])}
+
+  if(geo_scale=="county"){df %<>% dplyr::group_by(COUNTY, .add=TRUE)}
+  rs <- dplyr::summarize(df, !!result_name:=(as.function(!!srvyrf_name)(
                   !!as.name(target_var), vartype="se", level=0.95))) %>%                           # Generate the weighted statistic
-    mutate(!!sym(moe_name):=!!sym(se_name) * 1.645) %>% setDT() %>%                                # Margin of Error using standard error
-    setnames(grep("_label", colnames(.)), c("group_label")) %>%                                    # For clarity in multi-call datasets
-    .[, target_var:=as.character(target_var)] %>% .[, (se_name):=NULL]
-  if(!is.null(group_var)){
-    rs %<>% .[, group_var:=as.character(group_var)] %>%
-    setcolorder(c("target_var", "group_var", "group_label", as.character(result_name), as.character(moe_name)))
-  }else{
-    rs %<>% setcolorder(c("target_var", as.character(result_name), as.character(moe_name)))
-  }
+    dplyr::mutate(!!rlang::sym(moe_name):=!!rlang::sym(se_name) * 1.645) %>% data.table::setDT()
+
+
   return(rs)
 }
 
@@ -250,9 +254,12 @@ psrc_pums_stat <- function(stat_type, geo_scale, span, dyear, target_var, group_
 #'
 #' @return A table with the variable names, regional total, and margin of error
 #'
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
 #' @examples
+#' \dontrun{
 #' Sys.getenv("CENSUS_API_KEY")}
-# psrc_pums_total(1, 2019, "AGEP", "SEX")
+#' psrc_pums_total(1, 2019, "AGEP", "SEX")
 #' @export
 psrc_pums_total <- function(span, dyear, target_var, group_var=NULL, bin_defs=NULL){
   rs <- psrc_pums_stat(stat_type="total", geo_scale="region", span, dyear, target_var, group_var, bin_defs)
@@ -270,12 +277,15 @@ psrc_pums_total <- function(span, dyear, target_var, group_var=NULL, bin_defs=NU
 #'
 #' @author Michael Jensen
 #'
-#' @return A table with the variable names, regional count aka tally, and margin of error
+#' @return A table with the variable names, regional count (i.e. tally), and margin of error
 #'
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
 #' @examples
 #' \dontrun{
 #' Sys.getenv("CENSUS_API_KEY")}
-# psrc_pums_count(1, 2019, "AGEP", "SEX")
+#'
+#' psrc_pums_count(1, 2019, "AGEP", "SEX")
 #' @export
 psrc_pums_count <- function(span, dyear, target_var, group_var=NULL, bin_defs=NULL){
   rs <- psrc_pums_stat("tally", "region", span, dyear, target_var, group_var, bin_defs)
@@ -295,9 +305,13 @@ psrc_pums_count <- function(span, dyear, target_var, group_var=NULL, bin_defs=NU
 #'
 #' @return A table with the variable names, regional median, and margin of error
 #'
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
 #' @examples
+#' \dontrun{
 #' Sys.getenv("CENSUS_API_KEY")}
-# psrc_pums_median(1, 2019, "AGEP", "SEX")
+#'
+#' psrc_pums_median(1, 2019, "AGEP", "SEX")
 #' @export
 psrc_pums_median <- function(span, dyear, target_var, group_var=NULL, bin_defs=NULL){
   rs <- psrc_pums_stat(stat_type="median", geo_scale="region", span, dyear, target_var, group_var, bin_defs)
@@ -317,9 +331,12 @@ psrc_pums_median <- function(span, dyear, target_var, group_var=NULL, bin_defs=N
 #'
 #' @return A table with the variable names, regional mean, and margin of error
 #'
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
 #' @examples
 #' \dontrun{
 #' Sys.getenv("CENSUS_API_KEY")}
+#'
 # psrc_pums_mean(1, 2019, "AGEP", "SEX")
 #' @export
 psrc_pums_mean <- function(span, dyear, target_var, group_var=NULL, bin_defs=NULL){
@@ -339,9 +356,12 @@ psrc_pums_mean <- function(span, dyear, target_var, group_var=NULL, bin_defs=NUL
 #'
 #' @return A table with the variable names, regional mean, and margin of error
 #'
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
 #' @examples
 #' \dontrun{
 #' Sys.getenv("CENSUS_API_KEY")}
+#'
 # psrc_pums_ratio(1, 2019, "RNTP", "HINCP")
 #' @export
 psrc_pums_ratio <- function(span, dyear, numerator, denominator){
@@ -362,9 +382,12 @@ psrc_pums_ratio <- function(span, dyear, numerator, denominator){
 #'
 #' @return A table with the variable names, PSRC county totals, and margin of error
 #'
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
 #' @examples
 #' \dontrun{
 #' Sys.getenv("CENSUS_API_KEY")}
+#'
 # county_pums_total(1, 2019, "AGEP", "SEX")
 #' @export
 county_pums_total <- function(span, dyear, target_var, group_var=NULL, bin_defs=NULL){
@@ -385,10 +408,13 @@ county_pums_total <- function(span, dyear, target_var, group_var=NULL, bin_defs=
 #'
 #' @return A table with the variable names, PSRC county unit counts (i.e. tallies), and margin of error
 #'
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
 #' @examples
 #' \dontrun{
 #' Sys.getenv("CENSUS_API_KEY")}
-# county_pums_count(1, 2019, "AGEP", "SEX")
+#'
+#' county_pums_count(1, 2019, "AGEP", "SEX")
 #' @export
 county_pums_count <- function(span, dyear, target_var, group_var=NULL, bin_defs=NULL){
   rs <- psrc_pums_stat("tally", "county", span, dyear, target_var, group_var, bin_defs)
@@ -408,10 +434,13 @@ county_pums_count <- function(span, dyear, target_var, group_var=NULL, bin_defs=
 #'
 #' @return A table with the variable names, PSRC county medians, and margin of error
 #'
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
 #' @examples
 #' \dontrun{
 #' Sys.getenv("CENSUS_API_KEY")}
-# county_pums_median(1, 2019, "AGEP", "SEX")
+#'
+#' county_pums_median(1, 2019, "AGEP", "SEX")
 #' @export
 county_pums_median <- function(span, dyear, target_var, group_var=NULL, bin_defs=NULL){
   rs <- psrc_pums_stat("median", "county", span, dyear, target_var, group_var, bin_defs)
@@ -431,10 +460,13 @@ county_pums_median <- function(span, dyear, target_var, group_var=NULL, bin_defs
 #'
 #' @return A table with the variable names, PSRC county means, and margin of error
 #'
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
 #' @examples
 #' \dontrun{
 #' Sys.getenv("CENSUS_API_KEY")}
-# county_pums_mean(1, 2019, "AGEP", "SEX")
+#'
+#' county_pums_mean(1, 2019, "AGEP", "SEX")
 #' @export
 county_pums_mean <- function(span, dyear, target_var, group_var=NULL, bin_defs=NULL){
   rs <- psrc_pums_stat("mean", "county", span, dyear, target_var, group_var, bin_defs)
@@ -453,10 +485,13 @@ county_pums_mean <- function(span, dyear, target_var, group_var=NULL, bin_defs=N
 #'
 #' @return A table with the variable names, county ratios, and margin of error
 #'
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
 #' @examples
 #' \dontrun{
 #' Sys.getenv("CENSUS_API_KEY")}
-# psrc_pums_ratio(1, 2019, "RNTP", "HINCP")
+#'
+#' psrc_pums_ratio(1, 2019, "RNTP", "HINCP")
 #' @export
 psrc_pums_ratio <- function(span, dyear, numerator, denominator){
   rs <- psrc_pums_stat("ratio", "region", span, dyear, numerator, denominator)
