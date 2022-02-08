955ba88fe5a5c1dbf8f07771e2e379d97cbbbf95
Christy Lam <cyyllam@gmail.com>
2022-02-07 17:51:06 -0800

    update colname with original name

--- R/get-equity-geographies.R
+++ R/get-equity-geographies.R
@@ -2,7 +2,7 @@
 #'
 #' Retrieve a 0-1 variable for whether each tract in the region is in an equity geography or not
 #'
-#' @param equity_type string, The type of equity geography either '50' or 'regavg'
+#' @param equity_type string, The type of equity geography either '50' (50 percent) or 'regavg' (regional average)
 #' @param equity_group string, The group of people described by the equity geography, options are:
 #'                             'race',
 #'                             'poverty',
@@ -35,7 +35,7 @@ get_equity_geographies <- function(equity_type = '50', equity_group = 'race', ye
                   'elderly' = 'older',
                   'limited_english' = 'lep')
 
-  column_name <- paste('equity_geog', group, type, sep = "_")
+  column_name <- column_name_orig <- paste('equity_geog', group, type, sep = "_")
   if(nchar(column_name) > 31) column_name <- stringr::str_sub(column_name, 1, 31)
   column_names <- paste0("geoid,", column_name)
   arc_root <- "https://services6.arcgis.com/GWxg6t7KXELn1thE/arcgis/rest/services"
@@ -44,4 +44,6 @@ get_equity_geographies <- function(equity_type = '50', equity_group = 'race', ye
                 column_names,
                 "&f=pgeojson")
   equity_tracts <- sf::st_read(url) %>% sf::st_drop_geometry()
+  colnames(equity_tracts)[2] <- column_name_orig
+  return(equity_tracts)
 }
