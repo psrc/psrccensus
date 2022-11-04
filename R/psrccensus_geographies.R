8ba9ac70b5ecc0e904de9d90f3da3f0c274792a5
jensenmj <mjensen@psrc.org>
2022-11-04 14:06:57 -0700

    Fix prior commit.

--- R/psrccensus_geographies.R
+++ R/psrccensus_geographies.R
@@ -15,9 +15,9 @@ globalVariables(c(":=", "!!", ".", "enquos"))
 #' @importFrom dplyr filter select rename
 #' @export
 get_psrc_places <- function(year){
-  NAME <- COUNTYFP <- GEOID <- geometry <- psrc_region <- place_lookup <- counties <-NULL
+  psrc_region <- counties <- COUNTYFP <- place_lookup <- places <- GEOID <- NAME <- geometry <- NULL
   psrc_region <- tigris::counties("53", cb=TRUE) %>%
-    filter(COUNTYFP %in% psrc_counties) %>% dplyr::summarize() %>%
+    filter(COUNTYFP %in% c("033","035","053","061")) %>% dplyr::summarize() %>%
     st_transform(2285) # planar projection to allow intersect
   place_lookup <- tigris::places("53", year=year, cb=TRUE) %>%
     select(c(GEOID, NAME, geometry)) %>% st_transform(2285) %>% st_buffer(-1) %>% # To avoid any overlap
