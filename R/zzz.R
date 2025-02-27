02f9e438a55d55f887e4ca40bd93893b09c46f6e
jensenmj <mjensen@psrc.org>
2025-02-26 21:23:47 -0800

    Remotes:: is lighterweight.

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(lib, pkg){
   msg <- base::writeLines(c("Have you updated tidycensus since the release date of the data you'll use?",
-                            "devtools::install_github('walkerke/tidycensus')"))
+                            "remotes::install_github('walkerke/tidycensus')"))
   packageStartupMessage(msg)
 }
